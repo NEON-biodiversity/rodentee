8037f4b831e6f5fee8b56586a9cb7ad3d0c7b6be
Quentin Read <quentin.read@gmail.com>
2018-09-21 14:11:33 -0400

    Test a new binning algorithm to give all sites ten bin points Update the by species stan priors to enable a negative beta1 coefficient

--- fit_by_species_3models.r
+++ fit_by_species_3models.r
@@ -11,16 +11,18 @@ library(purrr)
 options(mc.cores = 3)
 rstan_options(auto_write = TRUE)
 
-model_d2p1 <- stan_model('~/Documents/GitHub/forestlight/code/exploratory/model_d2p1.stan')
+model_sp_h1 <- stan_model('~/Documents/GitHub/NEON_repos/rodentee/model_bysp_h1.stan')
+model_sp_h2 <- stan_model('~/Documents/GitHub/NEON_repos/rodentee/model_bysp_h2.stan')
+model_sp_h3 <- stan_model('~/Documents/GitHub/NEON_repos/rodentee/model_bysp_h3.stan')
 
 # Define function to fit this for each siteID.
-fit_by_species <- function(dat) {
+fit_by_species <- function(dat, mod) {
   standata <- with(dat, list(N = length(global_sp_avg_mass),
                              x = global_sp_avg_mass,
                              y = site_sp_total_metabolicRate,
                              x_min = min(global_sp_avg_mass),
                              x_max = max(global_sp_avg_mass)))
-  sampling(model_d2p1, data = standata, iter = 5000, warmup = 4000, chains = 3, pars = c('log_lik_dens', 'log_lik_prod'), include = FALSE)
+  sampling(mod, data = standata, iter = 5000, warmup = 4000, chains = 3)
   
 }
 
@@ -30,19 +32,87 @@ n_individuals <- mammal_data %>%
   group_by(siteID, year) %>%
   summarize(n=n(), site_richness = length(unique(taxonID)), xmin = min(weight), xmax = max(weight))
 
-all_fits <- mammal_sp_sums %>%
+all_fits_m1 <- mammal_sp_sums %>%
   left_join(n_individuals) %>%
   filter(year == 2016, n >= 50, site_richness >=4) %>%
-  do(fit = fit_by_species(.))
+  do(fit = fit_by_species(., model_sp_h1))
+
+all_fits_m2 <- mammal_sp_sums %>%
+  left_join(n_individuals) %>%
+  filter(year == 2016, n >= 50, site_richness >=4) %>%
+  do(fit = fit_by_species(., model_sp_h2))
+
+all_fits_m3 <- mammal_sp_sums %>%
+  left_join(n_individuals) %>%
+  filter(year == 2016, n >= 50, site_richness >=4) %>%
+  do(fit = fit_by_species(., model_sp_h3))
 
 # Extract the fitted values and their credible intervals, for plotting.
 source('~/Documents/GitHub/forestlight/stan/piecewise_workflow/extraction_functions_piecewise.r')
 
-parnames <- c('alpha_low', 'alpha_high', 'beta0', 'beta1', 'tau')
+parnames <- list(c('alpha', 'beta0', 'beta1'),
+                 c('alpha_low', 'alpha_high', 'beta0', 'beta1', 'tau'),
+                 c('alpha_low', 'alpha_mid', 'alpha_high', 'beta0', 'beta1', 'tau_low', 'tau_high'))
 
-all_pars <- all_fits %>%
+all_pars_m1 <- all_fits_m1 %>%
   group_by(siteID) %>%
-  do(param_values(.$fit[[1]], parnames))
+  do(param_values(.$fit[[1]], parnames[[1]]))
+
+
+all_pars_m2 <- all_fits_m2 %>%
+  group_by(siteID) %>%
+  do(param_values(.$fit[[1]], parnames[[2]]))
+
+
+all_pars_m3 <- all_fits_m3 %>%
+  group_by(siteID) %>%
+  do(param_values(.$fit[[1]], parnames[[3]]))
+
+# Extract log likelihood values and use these to get information criteria
+
+get_ic <- function(fit) {
+  ll_dens <- extract_log_lik(fit, 'log_lik_dens')
+  waic_dens <- waic(ll_dens)
+  loo_dens <- loo(ll_dens)
+  data.frame(WAIC = waic_dens$estimates['waic','Estimate'],
+             se_WAIC = waic_dens$estimates['waic','SE'],
+             LOOIC = loo_dens$estimates['looic','Estimate'],
+             se_LOOIC = loo_dens$estimates['looic','SE'])
+}
+
+all_ic_m1 <- all_fits_m1 %>%
+  group_by(siteID) %>%
+  do(get_ic(.$fit[[1]]))
+
+all_ic_m2 <- all_fits_m2 %>%
+  group_by(siteID) %>%
+  do(get_ic(.$fit[[1]]))
+
+all_ic_m3 <- all_fits_m3 %>%
+  group_by(siteID) %>%
+  do(get_ic(.$fit[[1]]))
+
+all_ic <- cbind(model = rep(1:3, each = nrow(all_fits_m1)), rbind(all_ic_m1, all_ic_m2, all_ic_m3))
+
+# Get best model
+get_best_model <- function(dat) {
+  # remove a model from list of best models if its interval is entirely worse than any other model
+  dat <- dat %>%
+    arrange(model) %>%
+    mutate(min = LOOIC - se_LOOIC, max = LOOIC + se_LOOIC)
+  best_models <- 1:3
+  for (i in 1:3) {
+    if (any(dat$min[i] > dat$max[-i])) {
+      best_models <- best_models[best_models != i]
+    }
+  }
+  # return the lowest number from remaining list
+  return(data.frame(best = min(best_models)))
+}
+
+all_best <- all_ic %>%
+  group_by(siteID) %>%
+  do(get_best_model(.))
 
 # Get mass values where we want to find the fitted values
 mass_limits <- range(mammal_sp_sums$global_sp_avg_mass)
@@ -54,25 +124,52 @@ min_n_2016 <- mammal_sp_sums %>%
   group_by(siteID) %>%
   summarize(x_min = min(global_sp_avg_mass), total_energy = sum(site_sp_total_metabolicRate), site_richness = n())
   
-all_fitted <- all_fits %>%
+all_fitted_m1 <- all_fits_m1 %>%
   left_join(min_n_2016) %>%
   group_by(siteID) %>%
-  do(fitted_predicted_values(.$fit[[1]], x_pred, dens_form = 2, prod_form = 1, x_min = .$x_min[1], n_indiv = .$site_richness[1], total_prod = .$total_energy[1], pars_to_get = parnames))
+  do(fitted_predicted_values(.$fit[[1]], x_pred, dens_form = 1, prod_form = 1, x_min = .$x_min[1], n_indiv = .$site_richness[1], total_prod = .$total_energy[1], pars_to_get = parnames[[1]]))
+
+all_fitted_m2 <- all_fits_m2 %>%
+  left_join(min_n_2016) %>%
+  group_by(siteID) %>%
+  do(fitted_predicted_values(.$fit[[1]], x_pred, dens_form = 2, prod_form = 1, x_min = .$x_min[1], n_indiv = .$site_richness[1], total_prod = .$total_energy[1], pars_to_get = parnames[[2]]))
+
+all_fitted_m3 <- all_fits_m3 %>%
+  left_join(min_n_2016) %>%
+  group_by(siteID) %>%
+  do(fitted_predicted_values(.$fit[[1]], x_pred, dens_form = 3, prod_form = 1, x_min = .$x_min[1], n_indiv = .$site_richness[1], total_prod = .$total_energy[1], pars_to_get = parnames[[3]]))
+
 
 # Edit fitted value data frame to change the names from the BCI names to mammal names
-names(all_fitted)[2] <- 'mass'
-all_fitted <- all_fitted %>%
+names(all_fitted_m1)[2] <- 'mass'
+names(all_fitted_m2)[2] <- 'mass'
+names(all_fitted_m3)[2] <- 'mass'
+
+all_fitted_m1 <- all_fitted_m1 %>%
   mutate(variable = factor(variable, labels = c('richness per size', 'energy per species', 'energy per species fitted', 'energy per size', 'energy per size fitted'))) %>%
   filter(variable %in% c('richness per size', 'energy per species', 'energy per size'))
+
+all_fitted_m2 <- all_fitted_m2 %>%
+  mutate(variable = factor(variable, labels = c('richness per size', 'energy per species', 'energy per species fitted', 'energy per size', 'energy per size fitted'))) %>%
+  filter(variable %in% c('richness per size', 'energy per species', 'energy per size'))
+
+all_fitted_m3 <- all_fitted_m3 %>%
+  mutate(variable = factor(variable, labels = c('richness per size', 'energy per species', 'energy per species fitted', 'energy per size', 'energy per size fitted'))) %>%
+  filter(variable %in% c('richness per size', 'energy per species', 'energy per size'))
+
 
 # Create spiffy plot.
 
 # Reorder factors
-all_fitted <- all_fitted %>%
+all_fitted_m1 <- all_fitted_m1 %>%
+  mutate(variable = factor(variable, levels = c('energy per species', 'richness per size', 'energy per size')))
+all_fitted_m2 <- all_fitted_m2 %>%
+  mutate(variable = factor(variable, levels = c('energy per species', 'richness per size', 'energy per size')))
+all_fitted_m3 <- all_fitted_m3 %>%
   mutate(variable = factor(variable, levels = c('energy per species', 'richness per size', 'energy per size')))
 
 # Just show first three sites
-ggplot(all_fitted %>% filter(siteID %in% c('BART','BLAN','CLBJ')), aes(x = mass)) +
+ggplot(all_fitted_m1 %>% filter(siteID %in% c('BART','BLAN','CLBJ')), aes(x = mass)) +
   facet_grid(siteID ~ variable) +
   geom_line(aes(y = q50), size = 1) +
   geom_line(aes(y = q025), linetype = 'dotted') +
@@ -123,7 +220,7 @@ energy_logbins <- mammal_sp_sums %>%
 richness_logbins <- mammal_sp_sums %>%
   do(logbin_setedges(x = .$global_sp_avg_mass, y = rep(1, nrow(.)), bin_edges = bin_bounds))
 
-ggplot(all_fitted %>% filter(siteID %in% c('BART','BLAN','CLBJ')), aes(x = mass)) +
+ggplot(all_fitted_m3 %>% filter(siteID %in% c('BART','BLAN','CLBJ')), aes(x = mass)) +
   facet_grid(siteID ~ variable) +
   geom_point(data = energy_logbins %>% 
                mutate(variable = 'energy per size') %>%
